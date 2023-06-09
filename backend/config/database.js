82e7d5e432fe339d8c4ad2c479b32d11ddbc8f36
Kevin Vu <kevintvu123@gmail.com>
2023-06-09 16:46:41 -0400

    sequelize setup complete

---
+++ backend/config/database.js
@@ -0,0 +1,24 @@
+// When app is deployed to production, database will be read from a URL path instead of local db file
+// Using PostgreSQL in production rather than SQLite3 as SQL DBMS
+const config = require('./index');
+
+module.exports = {
+  development: {
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
