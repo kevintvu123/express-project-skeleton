b51f9cb188bd166c098ad113e7149efba2a430eb
Kevin Vu <kevintvu123@gmail.com>
2023-06-09 18:05:03 -0400

    Added route to reset CSRF token

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function (req, res) {
-    //Setting cookie on response w/name of XSRF-TOKEN to val of req.csrfToken method
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// // Test Route
+// router.get('/hello/world', function (req, res) {
+//     //Setting cookie on response w/name of XSRF-TOKEN to val of req.csrfToken method
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+// Add a XSRF-TOKEN cookie. Allows dev to reset CSRF token cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
 
 module.exports = router;
