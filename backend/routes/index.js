4f98c4a8b316084576e3d1ad2bf6bc543ef1f854
Kevin Vu <kevintvu123@gmail.com>
2023-06-19 15:42:21 -0400

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api')
 
-// // Test Route
+//All the URLS of the routes in the api router will be prefixed with /api
+router.use('/api', apiRouter)
+
+// Test Route
 // router.get('/hello/world', function (req, res) {
 //     //Setting cookie on response w/name of XSRF-TOKEN to val of req.csrfToken method
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.send('Hello World!');
+    // res.send('Hello World!');
 // });
 
 // Add a XSRF-TOKEN cookie. Allows dev to reset CSRF token cookie
