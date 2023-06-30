02e258ba764f87c81b6323e03fb33ee7e6e45f96
Kevin Vu <kevintvu123@gmail.com>
2023-06-30 17:42:11 -0400

    Add backend endpoint for signup and get current

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -33,9 +33,23 @@ router.post(
 router.delete(
     '/',
     (_req, res) => {
-      res.clearCookie('XSRF-TOKEN');
-      return res.json({ message: 'success' });
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
     }
-  );
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
 
 module.exports = router;
